0e96e8ebfc3d6676e7301ff5e54cdc3a1c7ee988
pythexcel <atul.etech2011@gmail.com>
2023-04-04 15:15:11 +0530

    instances, placements and mediations

--- ironsource/index-ironsource.js
+++ ironsource/index-ironsource.js
@@ -13,6 +13,7 @@ const auth = async (secretkey,refreshToken)=>{
     const response = await axios.get(authUrl , configs);
     if(response.status !== 200) return false;
     axios.defaults.headers.common['Authorization'] = `Bearer ${response.data}`;
+    console.log(response.data, 'token')
     return response.data;
 }
 
@@ -24,15 +25,32 @@ const createAppApiUrl = 'https://platform.ironsrc.com/partners/publisher/applica
     return response.data;
 }
 
+const createInstances = async (app)=>{
+    const {appKey} = app;
+    const createInstanceApiUrl = 'https://platform.ironsrc.com/partners/publisher/instances/v3';
+    const data = config.instanceData;
+    data.appKey = appKey;
+    const response = await axios.post(createInstanceApiUrl,data);
+    if(response.status !== 200) return false;
+    return response.data;
+}
+
 const createPlacements = async (app)=>{
     const {appKey} = app;
     const createPlacementApiUrl = 'https://platform.ironsrc.com.com/partners/publisher/placements/v1';
     const data = config.placementData;
-    data.appKey = appKey
-    const agent = new https.Agent({  
-        rejectUnauthorized: false
-      });
-    const response = await axios.post(createPlacementApiUrl,data,{ httpsAgent: agent });
+    data.appKey = appKey;
+    const response = await axios.post(createPlacementApiUrl,data);
+    if(response.status !== 200) return false;
+    return response.data;
+}
+
+const createMediation = async (app)=>{
+    const {appKey} = app;
+    const createMediationApiUrl = 'https://platform.ironsrc.com/partners/publisher/mediation/management/v2';
+    const data = config.mediationData;
+    data.appKey = appKey;
+    const response = await axios.post(createMediationApiUrl,data);
     if(response.status !== 200) return false;
     return response.data;
 }
@@ -48,7 +66,19 @@ const createPlacements = async (app)=>{
         console.log("An Error occured while creating a app");
         return false
     }
-    await createPlacements(app);
-
+    const {useInstanceData, usePlacementData, useMediationData} = config;
     
+    if(useInstanceData){
+        const instances = await createInstances(app);
+        if(!instances) console.log('An Error occured while creating Instances');
+    } 
+    if(usePlacementData){
+        const placements = await createPlacements(app);
+        if(!placements) console.log('An Error occured while creating Placements');
+    } 
+    if(useMediationData){
+        const mediation = await createMediation(app);
+        if(!mediation) console.log('An Error occured while creating Mediations')
+    }
+
 })();
