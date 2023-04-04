05c13087a752a7f3fbcdf6be63ec075d039301d6
pythexcel <atul.etech2011@gmail.com>
2023-04-04 13:35:42 +0530

    remove comments

--- max-ads/index-maxads.js
+++ max-ads/index-maxads.js
@@ -1,6 +1,5 @@
 const config = require('./config-maxads.json');
 const axios = require('axios');
-const https = require('https')
 axios.defaults.headers.common['Api-Key'] = config.managementApiKey;
 
 const createApp = async ()=>{
@@ -8,18 +7,7 @@ const createAppApiUrl = 'https://o.applovin.com/mediation/v1/ad_unit';
     const data = config.appData; 
     const response = await axios.post(createAppApiUrl,data);
     if(response.status !== 200) return false;
-    console.log(response.data)
     return response.data;
-    //response
-    // {
-    //     "id": "1234567890abcdef",
-    //     "name": "My Inter Ad Unit",
-    //     "platform": "ios",
-    //     "package_name": "com.test.app",
-    //     "ad_format": "INTER",
-    //     "has_active_experiment": false, x
-    //     "disabled": false x
-    //  }
 }
 
 const configApp = async (app)=>{
@@ -28,7 +16,7 @@ const configApp = async (app)=>{
     const data = config.appConfig; 
     const response = await axios.post(createAppApiUrl,requestData);
     if(response.status !== 200) return false;
-    console.log(response.data)
+    console.log(response.data);
     return response.data;
 }
 
