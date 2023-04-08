38b8f81165e5ba334e57369c33e939e2a598e9b6
pythexcel <atul.etech2011@gmail.com>
2023-04-08 15:40:23 +0530

    inmobi script completed

--- inmobi/index.js
+++ inmobi/index.js
@@ -29,113 +29,135 @@ const windowSet = async (page, name, value) => {
 };
 
 ////////////////
-const login = async ({page, email, password})=>{
-  await windowSet(page, 'pass', password)
-  await page.goto('https://publisher.inmobi.com/signup', { waitUntil: 'networkidle0' });
+const login = async ({ page, email, password }, reAttempt) => {
+  try {
+    await windowSet(page, 'pass', password)
+    await page.goto('https://publisher.inmobi.com/signup', { waitUntil: 'networkidle0' });
 
-  // Set screen size
-  await page.setViewport({ width: 1080, height: 1024 });
-  console.log('logging In....')
-  await page.waitForSelector('button');
-  await page.click('button');
-  await page.waitForSelector('#email');
-  await page.type('#email', email);
-  await page.$eval('#btn-next', async (el) => {
-    console.log(el)
-    await setTimeout(() => {
-      el.click()
-    }, 1000)
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
+    console.log('logging In....')
+    await page.waitForSelector('button');
+    await page.click('button');
+    await page.waitForSelector('#email');
+    await page.type('#email', email);
+    await page.$eval('#btn-next', async (el) => {
+      await setTimeout(() => {
+        el.click()
+      }, 1000)
 
-  })
+    })
 
-  await waitForResponse(page, `https://iam.inmobi.com/iam/v3/user/getAuth0UserDetails?email=${email}`);
-  await page.exposeFunction('#password', () => { });
-  await page.waitForSelector('#password');
-  await page.$eval('#password', async (el) => {
-    await setTimeout(() => {
-      el.value = pass;
-    }, 1000)
-  });
-  await page.$eval('#btn-login', async (el) => {
-    await setTimeout(() => {
-      el.click()
-    }, 1000)
-  });
-  await page.waitForSelector('.css-1wuxrsi');
-  console.log('logged In')
+    await waitForResponse(page, `https://iam.inmobi.com/iam/v3/user/getAuth0UserDetails?email=${email}`);
+    await page.exposeFunction('#password', () => { });
+    await page.waitForSelector('#password');
+    await page.$eval('#password', async (el) => {
+      await setTimeout(() => {
+        el.value = pass;
+      }, 1000)
+    });
+    await page.$eval('#btn-login', async (el) => {
+      await setTimeout(() => {
+        el.click()
+      }, 1000)
+    });
+    await page.waitForSelector('.css-1wuxrsi');
+    console.log('logged In')
+  }
+  catch (e) {
+    console.log('login failed!');
+    if (!reAttempt) reAttempt = 0;
+    if (reAttempt < 5) {
+      reAttempt += 1
+      console.log(`reattempt ${reAttempt} out of 5`);
+      return await login({ page, email, password }, reAttempt)
+    }
+    throw e;
+  }
+
 }
-const createApp = async ({ app_url, app_name, page }) => {
-  
-  console.log(await page.url())
-  console.log('Creating App...')
-  await page.$eval('.css-1wuxrsi', (el) => {
-    el.click()
-  })
-  await page.waitForSelector('.css-1c2j7dd');
-  await page.$eval('.css-1c2j7dd', (el) => {
-    el.click()
-  })
-  await page.waitForSelector('.field__input');
-  await delay(2000)
-  await page.type(".field__input", app_url)
-  await waitForResponse(page, "https://publisher.inmobi.com/api/graphql");
-  const inputs = await page.$$(".field__input")
-  inputs[1].click({ clickCount: 3 })
-  await delay(2000)
-  inputs[1].type(app_name);
-  await page.waitForSelector('.css-1wuxrsi');
-  await page.$eval('.css-1wuxrsi', (el) => {
-    el.click()
-  })
-  await waitForResponse(page, "https://publisher.inmobi.com/api/graphql");
-  await page.waitForSelector('#radio-gdpr-true');
-  await page.waitForSelector('#radio-location-true');
-  await waitForResponse(page, "https://publisher.inmobi.com/api/graphql");
-  await page.waitForSelector('#radio-gdpr-true');
-  await page.$eval('#radio-gdpr-true', (el) => {
-    el.click()
-  })
-  await page.waitForSelector('#radio-location-true');
-  await page.$eval('#radio-location-true', (el) => {
-    el.click()
-  })
-  await delay(2000)
-  await page.waitForSelector('.css-1wuxrsi');
-  await page.$eval('.css-1wuxrsi', (el) => {
-    el.click()
-  })
-  await page.waitForNavigation();
-  await page.waitForSelector('.css-ceuxau');
-  const pageUrl = await page.url();
-  const appId = pageUrl.split('/')[pageUrl.split('/').length-1];
-  console.log('App created:',appId)
-  return appId
+const createApp = async ({ app_url, app_name, page }, reAttempt) => {
+  try {
+    console.log('Creating App...')
+    await page.goto('https://publisher.inmobi.com/my-inventory/app-and-placements', { waitUntil: 'networkidle0' });
+    await page.waitForSelector('.css-1wuxrsi');
+    await page.$eval('.css-1wuxrsi', (el) => {
+      el.click()
+    })
+    await page.waitForSelector('.css-1c2j7dd');
+    await page.$eval('.css-1c2j7dd', (el) => {
+      el.click()
+    })
+    await page.waitForSelector('.field__input');
+    await delay(2000)
+    await page.type(".field__input", app_url)
+    await waitForResponse(page, "https://publisher.inmobi.com/api/graphql");
+    const inputs = await page.$$(".field__input")
+    inputs[1].click({ clickCount: 3 })
+    await delay(2000)
+    inputs[1].type(app_name);
+    await page.waitForSelector('.css-1wuxrsi');
+    await page.$eval('.css-1wuxrsi', (el) => {
+      el.click()
+    })
+    await waitForResponse(page, "https://publisher.inmobi.com/api/graphql");
+    await page.waitForSelector('#radio-gdpr-true');
+    await page.waitForSelector('#radio-location-true');
+    await waitForResponse(page, "https://publisher.inmobi.com/api/graphql");
+    await page.waitForSelector('#radio-gdpr-true');
+    await page.$eval('#radio-gdpr-true', (el) => {
+      el.click()
+    })
+    await page.waitForSelector('#radio-location-true');
+    await page.$eval('#radio-location-true', (el) => {
+      el.click()
+    })
+    await delay(2000)
+    await page.waitForSelector('.css-1wuxrsi');
+    await page.$eval('.css-1wuxrsi', (el) => {
+      el.click()
+    })
+    await page.waitForNavigation();
+    await page.waitForSelector('.css-ceuxau');
+    const pageUrl = await page.url();
+    const appId = pageUrl.split('/')[pageUrl.split('/').length - 1];
+    console.log('App created:', appId)
+    return appId;
+  } catch (e) {
+    console.log('App creation failed!');
+    if (!reAttempt) reAttempt = 0;
+    if (reAttempt < 5) {
+      reAttempt += 1
+      console.log(`reattempt ${reAttempt} out of 5`);
+      return await createApp({ page, email, password }, reAttempt)
+    }
+    throw e;
+  }
+
 }
 
-const createPlacements = async ({page, appId, placements})=>{
-  await page.goto(`https://publisher.inmobi.com/my-inventory/app-and-placements/create-placement/${appId}`, { waitUntil: 'networkidle0' });
+const createPlacements = async ({ page, appId, placements }, reAttempt) => {
+  try{
+    await page.goto(`https://publisher.inmobi.com/my-inventory/app-and-placements/create-placement/${appId}`, { waitUntil: 'networkidle0' });
   await page.waitForSelector('.css-cb9ru8');
   const placementElements = await page.$$(".css-cb9ru8")
-  const setPlacementConfig = async (placementName, abid, partner, testMode)=>{
-    console.log(placementName,'..............')
+  const setPlacementConfig = async (placementName, abid, partner, testMode) => {
     await page.waitForSelector('.form-control')
-    console.log('typing')
     await page.type('.form-control', placementName);
-    console.log('typing done')
-    let dropdown;
-    if(abid){
+    let dropDowns;
+    if (abid) {
       dropDowns = await page.$$('.dropdown');
       await dropDowns[0].click()
       await page.waitForSelector('.dropdown__list-item')
-      await page.$eval('.dropdown__list-item',el=>el.click());
+      await page.$eval('.dropdown__list-item', el => el.click());
       await delay(1000);
       dropDowns = await page.$$('.dropdown');
       await dropDowns[1].click();
       await page.waitForSelector('.dropdown__list-item');
       let dropDownsListItems = await page.$$('.dropdown__list-item');
-      for(listItem of dropDownsListItems){
+      for (listItem of dropDownsListItems) {
         const text = await page.evaluate(el => el.textContent, listItem)
-        if(text === partner){
+        if (text === partner) {
           await listItem.click();
           break;
         }
@@ -143,20 +165,20 @@ const createPlacements = async ({page, appId, placements})=>{
       await delay(1000)
       await dropDowns[2].click();
       dropDownsListItems = await page.$$('.dropdown__list-item');
-      for(listItem of dropDownsListItems){
+      for (listItem of dropDownsListItems) {
         const text = await page.evaluate(el => el.textContent, listItem)
-        if(text === testMode){
+        if (text === testMode) {
           await listItem.click();
           break;
         }
       }
-    }else{
+    } else {
       dropDowns = await page.$$('.dropdown');
       await dropDowns[1].click();
       dropDownsListItems = await page.$$('.dropdown__list-item');
-      for(listItem of dropDownsListItems){
+      for (listItem of dropDownsListItems) {
         const text = await page.evaluate(el => el.textContent, listItem)
-        if(text === testMode){
+        if (text === testMode) {
           await listItem.click();
           break;
         }
@@ -167,28 +189,38 @@ const createPlacements = async ({page, appId, placements})=>{
     })
   }
   const indexOfPlacements = {
-    "interstitial":0,
-    "banner":1,
-    "rewarded":2,
-    "inStream":3,
-    "native":4
+    "interstitial": 0,
+    "banner": 1,
+    "rewarded": 2,
+    "inStream": 3,
+    "native": 4
   }
-  for(placement of placements){
-    const {type, placementName, audienceBidding, partner, testMode} = placement;
+  for (placement of placements) {
+    const { type, placementName, audienceBidding, partner, testMode } = placement;
     await placementElements[indexOfPlacements[type]].$eval('button', el => el.click());
     await setPlacementConfig(placementName, audienceBidding, partner, testMode);
+  }
+  return true
+  }catch(e){
+    console.log('Placement creation failed!');
+    if (!reAttempt) reAttempt = 0;
+    if (reAttempt < 5) {
+      reAttempt += 1
+      console.log(`reattempt ${reAttempt} out of 5`);
+      return await createPlacements({ page, email, password }, reAttempt)
+    }
+    throw e;
   }
 }
 
 
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: config.isHeadLess});
+  const browser = await puppeteer.launch({ headless: config.isHeadLess });
   const page = await browser.newPage();
-  const args = {...config,page}
+  const args = { ...config, page }
   await login(args);
   const appId = await createApp(args)
-  
-  await createPlacements({...args, appId})
+  await createPlacements({ ...args, appId })
   await browser.close();
 })();
